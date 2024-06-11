ab98d70585621aade46052007565e12e965976a3
it <kyle9742@naver.com>
2024-06-11 15:29:34 +0900

    app css 추가

--- src/App.js
+++ src/App.js
@@ -3,30 +3,44 @@ import MovieList from './components/MovieList';
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
+import MovieListHead from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
+import MovieListHeading from './components/MovieListHeading';
 function App() {
   const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
 
   // 검색어로 영화데이터 요청
   // async, await => 데이터를 받는데 기다려줌
   const getMovieRequest = async (searchValue) => {
-    const url = `http://www.omdbapi.com/?apikey=cae26eeb&s=${searchValue}`;
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=cae26eeb`;
     const response = await fetch(url);
     const responseJson = await response.json();
     console.log(responseJson.Search);
+
+    if(responseJson.Search) {
+      setMovies(responseJson.Search)
+    }
   }
 
   useEffect(() => {
+    if(searchValue.length > 2) {
+      getMovieRequest(searchValue);
+    }
+  }, [searchValue]);
 
-  }, [])
-
-  getMovieRequest('amazing');
 
   return (
     <div className='container-fluid movie-app'>
-      <div className='row'>
-        <MovieList movies={movies} />
-      </div>
+    <div className='row align-items-center my-4'>
+      <MovieListHeading heading='제목' />
+      <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
     </div>
+
+    <div className="row">
+      <MovieList movies={movies} />
+    </div>
+  </div>
   );
 }
 
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,9 +4,13 @@ const MovieList = (props) => {
   return (
     <>
         {props.movies.map((movie) => (
-            <div className='d-flex m-3' key={movie.imdbID}>
-            <img src={movie.Poster} alt="movie"></img>
-            </div>
+           <div className='image-container d-flex m-3' key={movie.imdbID}>
+           <img src={movie.Poster} alt='movie'></img>
+           <div className='overlay d-flex align-items-center justify-content-center'>
+             <span className='me-2'>선호작 추가</span>
+             <span>❤️</span>
+           </div>
+         </div>
         ))}
     </>
   )
