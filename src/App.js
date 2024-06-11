9ca260f5cbdceceb25ba14177e5e227d54a3491f
it <kyle9742@naver.com>
2024-06-11 14:31:52 +0900

    영화 데이터 저장

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import './App.css';
+import { useEffect, useState } from 'react';
+import MovieList from './components/MovieList';
 
+import "bootstrap/dist/css/bootstrap.min.css"
+import './App.css';
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  // 검색어로 영화데이터 요청
+  // async, await => 데이터를 받는데 기다려줌
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?apikey=cae26eeb&s=${searchValue}`;
+    const response = await fetch(url);
+    const responseJson = await response.json();
+    console.log(responseJson.Search);
+  }
+
+  useEffect(() => {
+
+  }, [])
+
+  getMovieRequest('amazing');
+
   return (
-    <div>
-      헬로우
+    <div className='container-fluid movie-app'>
+      <div className='row'>
+        <MovieList movies={movies} />
+      </div>
     </div>
   );
 }
